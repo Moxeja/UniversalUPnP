06d19ed3d3d946d020cc67f50d2e77cb92399a3b
Moxeja <1824492+Moxeja@users.noreply.github.com>
2019-09-12 23:51:22 +0100

    Add multi-threading for stopping upnpservice

--- src/com/moxeja/uupnp/MappingEntry.java
+++ src/com/moxeja/uupnp/MappingEntry.java
@@ -40,39 +40,45 @@ public class MappingEntry {
 	}
 	
 	public void startUPnP() throws Exception, UnknownHostException {
-		if (upnpservice != null) {
+		synchronized (name) {
+			if (upnpservice != null) {
+				running = true;
+				return;
+			}
+			
+			// Get local IP
+			String internalIP;
+			internalIP = InetAddress.getLocalHost().getHostAddress();
+			
+			// Setup portmapping to use with upnpservice
+			PortMapping[] portList;
+			if (protocol == Protocols.UDP) {
+				portList = new PortMapping[] {new PortMapping(port, internalIP, PortMapping.Protocol.UDP, name)};
+			} else if (protocol == Protocols.TCP) {
+				portList = new PortMapping[] {new PortMapping(port, internalIP, PortMapping.Protocol.TCP, name)};
+			} else if (protocol == Protocols.UDP_TCP) {
+				portList = new PortMapping[] {new PortMapping(port, internalIP, PortMapping.Protocol.UDP, name),
+						new PortMapping(port, internalIP, PortMapping.Protocol.TCP, name)};
+			} else {
+				throw new Exception("Unknown Protocol Type: "+protocol);
+			}
+			
+			// Use Jetty implementation to stop errors
+			upnpservice = new UpnpServiceImpl(new JettyUPnPConfiguration(), new PortMappingListener(portList));
+			upnpservice.getControlPoint().search();
 			running = true;
-			return;
 		}
-		
-		// Get local IP
-		String internalIP;
-		internalIP = InetAddress.getLocalHost().getHostAddress();
-		
-		// Setup portmapping to use with upnpservice
-		PortMapping[] portList;
-		if (protocol == Protocols.UDP) {
-			portList = new PortMapping[] {new PortMapping(port, internalIP, PortMapping.Protocol.UDP, name)};
-		} else if (protocol == Protocols.TCP) {
-			portList = new PortMapping[] {new PortMapping(port, internalIP, PortMapping.Protocol.TCP, name)};
-		} else if (protocol == Protocols.UDP_TCP) {
-			portList = new PortMapping[] {new PortMapping(port, internalIP, PortMapping.Protocol.UDP, name),
-					new PortMapping(port, internalIP, PortMapping.Protocol.TCP, name)};
-		} else {
-			throw new Exception("Unknown Protocol Type: "+protocol);
-		}
-		
-		// Use Jetty implementation to stop errors
-		upnpservice = new UpnpServiceImpl(new JettyUPnPConfiguration(), new PortMappingListener(portList));
-		upnpservice.getControlPoint().search();
-		running = true;
 	}
 	
 	public void stopUPnP() {
-		if (upnpservice != null) {
-			upnpservice.shutdown();
-			upnpservice = null;
-			running = false;
+		if (upnpservice != null && running) {
+			new Thread(() -> {
+				synchronized (name) {
+					upnpservice.shutdown();
+					upnpservice = null;
+				}
+			}).start();
 		}
+		running = false;
 	}
 }
