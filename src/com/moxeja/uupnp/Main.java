35d4bd70150c320e2a9ad4225d8dbf92928add78
Moxeja <1824492+Moxeja@users.noreply.github.com>
2019-09-21 04:50:28 +0100

    Move unrelated logic from MainWindow

--- src/com/moxeja/uupnp/Main.java
+++ src/com/moxeja/uupnp/Main.java
@@ -1,12 +1,109 @@
 package com.moxeja.uupnp;
 
 import java.awt.EventQueue;
+import java.awt.GraphicsEnvironment;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
+import com.moxeja.uupnp.Logger.LogSeverity;
 import com.moxeja.uupnp.ui.MainWindow;
 
 public class Main {
+	
+	public static Logger LOGGER;
+	public static final String VERSION = "V1.3";
+	public static MappingList DATA;
+	
+	private static int getJavaVersion() {
+		String version = System.getProperty("java.version");
+		if (version.startsWith("1.")) {
+			version = version.substring(2, 3);
+		} else {
+			int dot = version.indexOf(".");
+			if (dot != -1) { version = version.substring(0, dot); }
+		}
+		return Integer.parseInt(version);
+	}
 
 	public static void main(String[] args) {
+		// Setup logging
+		LOGGER = new Logger(FileLocations.getLogFilename("uupnp"));
+		LOGGER.log(LogSeverity.INFO, "Uses GSON, Cling, Seamless and Jetty libraries from "
+				+ "https://github.com/google/gson, https://github.com/4thline/cling,"
+				+ "https://github.com/4thline/seamless and https://github.com/eclipse/jetty.project respectively.");
+		LOGGER.log(LogSeverity.INFO, "Running version: "+VERSION);
+		
+		try {
+			System.setErr(new PrintStream(new BufferedOutputStream(new FileOutputStream(FileLocations.getLogFilename("uupnp-err-output")))));
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+			LOGGER.log(LogSeverity.WARN, "Could not redirect error stream to file.");
+		}
+		
+		// Program cannot be run in headless mode since it is GUI based
+		if (GraphicsEnvironment.isHeadless()) {
+			LOGGER.log(LogSeverity.FATAL, "Application cannot be run in headless mode.");
+			LOGGER.close();
+			System.exit(1);
+		}
+		
+		// Java 8 is the minimum required
+		if (getJavaVersion() < 8) {
+			LOGGER.log(LogSeverity.FATAL, "At least Java 8 is required to run this software.");
+			LOGGER.close();
+			System.exit(1);
+		}
+		
+		// Add shutdown hook to ensure data gets written and finalised
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+				DATA.stopAll();
+				
+				try {
+					PrintWriter outwriter = new PrintWriter(new BufferedWriter(new FileWriter(FileLocations.getEntriesFilename())));
+					Gson gson = new GsonBuilder().setPrettyPrinting().create();
+					gson.toJson(DATA, outwriter);
+					outwriter.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				LOGGER.log(LogSeverity.INFO, "Closing program.");
+				LOGGER.close();
+			}
+		});
+		LOGGER.log(LogSeverity.INFO, "Added shutdown hook.");
+		
+		// Try to load entries from Json file
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(FileLocations.getEntriesFilename()));
+			Gson gs = new Gson();
+			DATA = gs.fromJson(br, MappingList.class);
+		} catch (JsonSyntaxException jse) {
+			LOGGER.log(LogSeverity.WARN, "Entries file was found, but was not formed correctly. Could not load data.");
+			DATA = new MappingList();
+		} catch (Exception e) {
+			LOGGER.log(LogSeverity.WARN, "Entries file could not be found. One will be created on application exit.");
+			DATA = new MappingList();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e1) {}
+			}
+		}
+		
 		// Start GUI
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
--- src/com/moxeja/uupnp/ui/MainWindow.java
+++ src/com/moxeja/uupnp/ui/MainWindow.java
@@ -3,21 +3,10 @@ package com.moxeja.uupnp.ui;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.GraphicsEnvironment;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.io.PrintWriter;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -31,13 +20,8 @@ import javax.swing.ListSelectionModel;
 import javax.swing.UIManager;
 import javax.swing.table.DefaultTableModel;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonSyntaxException;
-import com.moxeja.uupnp.FileLocations;
-import com.moxeja.uupnp.Logger;
-import com.moxeja.uupnp.MappingList;
 import com.moxeja.uupnp.Logger.LogSeverity;
+import com.moxeja.uupnp.Main;
 import com.moxeja.uupnp.datatypes.MappingEntry;
 import com.moxeja.uupnp.datatypes.PortInfo;
 
@@ -46,74 +30,12 @@ public class MainWindow {
 	private JFrame frmUniversalupnp;
 	private JTable table;
 	private DefaultTableModel tablemodel;
-	
-	public static Logger LOGGER;
-	public static final String VERSION = "V1.3";
-	private static MappingList DATA;
-
-	private static int getJavaVersion() {
-		String version = System.getProperty("java.version");
-		if (version.startsWith("1.")) {
-			version = version.substring(2, 3);
-		} else {
-			int dot = version.indexOf(".");
-			if (dot != -1) { version = version.substring(0, dot); }
-		}
-		return Integer.parseInt(version);
-	}
 
 	/**
 	 * Create the application.
 	 */
 	public MainWindow() {
-		// Setup logging
-		LOGGER = new Logger(FileLocations.getLogFilename("uupnp"));
-		LOGGER.log(LogSeverity.INFO, "Uses GSON, Cling, Seamless and Jetty libraries from "
-				+ "https://github.com/google/gson, https://github.com/4thline/cling,"
-				+ "https://github.com/4thline/seamless and https://github.com/eclipse/jetty.project respectively.");
-		LOGGER.log(LogSeverity.INFO, "Running version: "+VERSION);
-		
-		try {
-			System.setErr(new PrintStream(new BufferedOutputStream(new FileOutputStream(FileLocations.getLogFilename("uupnp-err-output")))));
-		} catch (FileNotFoundException e1) {
-			e1.printStackTrace();
-			LOGGER.log(LogSeverity.WARN, "Could not redirect error stream to file.");
-		}
-		
-		// Program cannot be run in headless mode since it is GUI based
-		if (GraphicsEnvironment.isHeadless()) {
-			LOGGER.log(LogSeverity.FATAL, "Application cannot be run in headless mode.");
-			LOGGER.close();
-			System.exit(1);
-		}
-		
-		// Java 8 is the minimum required
-		if (getJavaVersion() < 8) {
-			LOGGER.log(LogSeverity.FATAL, "At least Java 8 is required to run this software.");
-			LOGGER.close();
-			System.exit(1);
-		}
-		
-		// Add shutdown hook to ensure data gets written and finalised
-		Runtime.getRuntime().addShutdownHook(new Thread() {
-			public void run() {
-				DATA.stopAll();
-				
-				try {
-					PrintWriter outwriter = new PrintWriter(new BufferedWriter(new FileWriter(FileLocations.getEntriesFilename())));
-					Gson gson = new GsonBuilder().setPrettyPrinting().create();
-					gson.toJson(DATA, outwriter);
-					outwriter.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				LOGGER.log(LogSeverity.INFO, "Closing program.");
-				LOGGER.close();
-			}
-		});
-		LOGGER.log(LogSeverity.INFO, "Added shutdown hook.");
-		
-		LOGGER.log(LogSeverity.INFO, "Initialising window.");
+		Main.LOGGER.log(LogSeverity.INFO, "Initialising window.");
 		initialize();
 		frmUniversalupnp.setVisible(true);
 	}
@@ -126,8 +48,7 @@ public class MainWindow {
 		frmUniversalupnp.setVisible(true);
 		frmUniversalupnp.setSize(new Dimension(680, 400));
 		frmUniversalupnp.setResizable(false);
-		frmUniversalupnp.setName("frame");
-		frmUniversalupnp.setTitle("UniversalUPnP "+VERSION);
+		frmUniversalupnp.setTitle("UniversalUPnP "+Main.VERSION);
 		frmUniversalupnp.setBounds(100, 100, 680, 400);
 		frmUniversalupnp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmUniversalupnp.getContentPane().setLayout(null);
@@ -271,36 +192,16 @@ public class MainWindow {
 		});
 		popupMenu.add(mntmStopMapping);
 		
-		// Try to load entries from Json file
-		BufferedReader br = null;
-		try {
-			br = new BufferedReader(new FileReader(FileLocations.getEntriesFilename()));
-			Gson gs = new Gson();
-			DATA = gs.fromJson(br, MappingList.class);
-		} catch (JsonSyntaxException jse) {
-			LOGGER.log(LogSeverity.WARN, "Entries file was found, but was not formed correctly. Could not load data.");
-			DATA = new MappingList();
-		} catch (Exception e) {
-			LOGGER.log(LogSeverity.WARN, "Entries file could not be found. One will be created on application exit.");
-			DATA = new MappingList();
-		} finally {
-			if (br != null) {
-				try {
-					br.close();
-				} catch (IOException e1) {}
-			}
-		}
-		
 		// Show data in table
 		refreshTable();
-		LOGGER.log(LogSeverity.INFO, "Window initialisation complete.");
+		Main.LOGGER.log(LogSeverity.INFO, "Window initialisation complete.");
 	}
 	
 	private void refreshTable() {
 		while (tablemodel.getRowCount() > 0)
 			tablemodel.removeRow(0);
 		
-		for (MappingEntry entry : DATA.getEntryList()) {			
+		for (MappingEntry entry : Main.DATA.getEntryList()) {			
 			if (entry.getPorts() != null) {
 				StringBuilder sbPorts = new StringBuilder();
 				StringBuilder sbProtocols = new StringBuilder();
@@ -318,7 +219,7 @@ public class MainWindow {
 				tablemodel.addRow(temp);
 			}
 		}
-		LOGGER.log(LogSeverity.INFO, "Refreshed table.");
+		Main.LOGGER.log(LogSeverity.INFO, "Refreshed table.");
 	}
 	
 	private void btnStartMappingClicked() {
@@ -326,7 +227,7 @@ public class MainWindow {
 		if (selectedIndex == -1)
 			return;
 		
-		DATA.startEntry(selectedIndex);
+		Main.DATA.startEntry(selectedIndex);
 		refreshTable();
 	}
 	
@@ -335,16 +236,16 @@ public class MainWindow {
 		if (selectedIndex == -1)
 			return;
 		
-		DATA.stopEntry(selectedIndex);
+		Main.DATA.stopEntry(selectedIndex);
 		refreshTable();
 	}
 	
 	private void btnAddMappingClicked() {
-		MappingInputForm inputForm = new MappingInputForm(this.frmUniversalupnp);
+		MappingInputForm inputForm = new MappingInputForm(frmUniversalupnp);
 		inputForm.setVisible(true);
 		
 		if (inputForm.okClose) {
-			DATA.addEntry(new MappingEntry(inputForm.name, inputForm.ports));
+			Main.DATA.addEntry(new MappingEntry(inputForm.name, inputForm.ports));
 			refreshTable();
 		}
 		inputForm.dispose();
@@ -356,10 +257,10 @@ public class MainWindow {
 			return;
 		
 		// User confirmation
-		int option = JOptionPane.showConfirmDialog(this.frmUniversalupnp, "Are you sure you want to delete an entry?", 
+		int option = JOptionPane.showConfirmDialog(frmUniversalupnp, "Are you sure you want to delete an entry?", 
 				"Delete Entry", JOptionPane.OK_CANCEL_OPTION);
 		if (option == JOptionPane.OK_OPTION) {
-			DATA.deleteEntry(selectedIndex);
+			Main.DATA.deleteEntry(selectedIndex);
 			refreshTable();
 		}
 	}
