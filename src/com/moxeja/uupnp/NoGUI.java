c0a4a196fb798d346adce72d08ede41a3610807d
Moxeja <1824492+Moxeja@users.noreply.github.com>
2019-09-28 17:09:40 +0100

    Add command line mode

---
+++ src/com/moxeja/uupnp/NoGUI.java
@@ -0,0 +1,80 @@
+package com.moxeja.uupnp;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.moxeja.uupnp.Logger.LogSeverity;
+import com.moxeja.uupnp.datatypes.MappingEntry;
+import com.moxeja.uupnp.datatypes.PortInfo;
+import com.moxeja.uupnp.datatypes.Protocols;
+
+public class NoGUI {
+
+	private enum Commands {
+		Create,
+		Start,
+		Stop,
+		Unknown
+	}
+	
+	private Commands parseArg(String arg) {
+		switch (arg) {
+		case "-create":
+		case "--create":
+			return Commands.Create;
+		case "-start":
+		case "--start":
+			return Commands.Start;
+		case "stop":
+		case "quit":
+		case "exit":
+		case "q":
+			return Commands.Stop;
+		default:
+			return Commands.Unknown;
+		}
+	}
+	
+	public NoGUI(String[] args) {
+		Commands command = parseArg(args[0]);
+		if (command == Commands.Unknown) {
+			Main.LOGGER.log(LogSeverity.FATAL, "Unknown argument: " + args[0]);
+			Main.LOGGER.log(LogSeverity.INFO, "Available arguments: -create, -start");
+			return;
+		}
+		
+		if (command == Commands.Create) {	// Create a template file for editing
+			ArrayList<PortInfo> templatePorts = new ArrayList<PortInfo>();
+			templatePorts.add(new PortInfo(new Point(8080, 8080), Protocols.UDP));
+			templatePorts.add(new PortInfo(new Point(8081, 8085), Protocols.TCP));
+			templatePorts.add(new PortInfo(new Point(8090, 8090), Protocols.UDP_TCP));
+			
+			Main.LOGGER.log(LogSeverity.INFO, "A template file will be created at: " + FileLocations.getWorkingDir());
+			Main.DATA.addEntry(new MappingEntry("Template Entry", templatePorts));
+			return;
+		} else if (command == Commands.Start) {	// Start all mappings from entries file
+			if (Main.DATA.isEmpty()) {
+				Main.LOGGER.log(LogSeverity.FATAL, "No valid UPnP services in entries file.");
+				return;
+			} else {
+				Main.DATA.startAll();
+				Main.LOGGER.log(LogSeverity.INFO, "Type stop, quit, exit or q to stop the mappings.");
+				
+				boolean stop = false;
+				Scanner scanner = new Scanner(System.in);
+				do {
+					String input = scanner.next();
+					if (parseArg(input) == Commands.Stop) {
+						stop = true;
+					}
+				} while (!stop);
+				scanner.close();
+				return;
+			}
+		} else {
+			Main.LOGGER.log(LogSeverity.FATAL, "Available arguments: -create, -start");
+			return;
+		}
+	}
+}
