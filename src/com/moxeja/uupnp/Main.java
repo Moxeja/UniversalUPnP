befc4def6245d64bf5d6e1689aa9f55693b96b37
Moxeja <1824492+Moxeja@users.noreply.github.com>
2019-09-30 14:13:50 +0100

    Bump version to 1.6

--- src/com/moxeja/uupnp/Main.java
+++ src/com/moxeja/uupnp/Main.java
@@ -22,7 +22,7 @@ import com.moxeja.uupnp.ui.MainWindow;
 public class Main {
 	
 	public static Logger LOGGER;
-	public static final String VERSION = "V1.5";
+	public static final String VERSION = "V1.6";
 	public static MappingList DATA;
 	
 	private static int getJavaVersion() {
