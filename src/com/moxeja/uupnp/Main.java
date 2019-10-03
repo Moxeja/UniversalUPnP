d075ffa5900807ca8332ddef3faa26ebfb1c251f
Moxeja <1824492+Moxeja@users.noreply.github.com>
2019-10-03 16:30:00 +0100

    Bump version to 1.7.2

--- src/com/moxeja/uupnp/Main.java
+++ src/com/moxeja/uupnp/Main.java
@@ -22,7 +22,7 @@ import com.moxeja.uupnp.ui.MainWindow;
 public class Main {
 	
 	public static Logger LOGGER;
-	public static final String VERSION = "1.7.1";
+	public static final String VERSION = "1.7.2";
 	public static MappingList DATA;
 	
 	private static int getJavaVersion() {
