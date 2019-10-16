e1d852cf4d3adf94f4008bb6229ba43c2ff4340d
Moxeja <1824492+Moxeja@users.noreply.github.com>
2019-10-16 16:58:31 +0100

    Minor license header change

--- src/com/moxeja/uupnp/network/Protocols.java
+++ src/com/moxeja/uupnp/network/Protocols.java
@@ -2,7 +2,7 @@
  * Copyright (C) 2019  Moxeja
  * Protocols.java is part of UniversalUPnP
  *
- * UniversalUPnP is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
